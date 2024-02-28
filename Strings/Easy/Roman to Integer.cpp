1577b72852efc6033936fca00ef3ccd917eca26b
Vipanshu Suman <vipanshu.suman@gmail.com>
2024-02-28 09:17:51 +0530

    Update Roman to Integer.cpp

--- Strings/Easy/Roman to Integer.cpp
+++ Strings/Easy/Roman to Integer.cpp
@@ -1,12 +1,17 @@
-    int romanToInt(string s) {
-        unordered_map<char, int> roman = {{'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},{'C', 100}, {'D', 500}, {'M', 1000}};
-        int res = 0;
-        for (int i = 0; i < s.length(); ++i) {
-            if (i + 1 < s.length() && roman[s[i]] < roman[s[i + 1]]) {
-                res -= roman[s[i]];
-            } else {
-                res += roman[s[i]];
-            }
+int romanToInt(string s)
+{
+    unordered_map<char, int> roman = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+    int res = 0;
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if (i + 1 < s.length() && roman[s[i]] < roman[s[i + 1]])
+        {
+            res -= roman[s[i]];
         }
-        return res;
+        else
+        {
+            res += roman[s[i]];
+        }
     }
+    return res;
+}
