6594039ff434de631761a6b35dc4163d73c7766b
Vipanshu Suman <vipanshu.suman@gmail.com>
2024-05-01 21:10:12 +0530

    Create Add one to a number represented as Linked List.cpp

---
+++ LL/Easy/Add one to a number represented as Linked List.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition of linked list:
+ *
+ * class Node {
+ * public:
+ *      int data;
+ *      Node *next;
+ *      Node() {
+ *          this->data = 0;
+ *          this->next = NULL;
+ *      }
+ *      Node(int data) {
+ *          this->data = data;
+ *          this->next = NULL;
+ *      }
+ *      Node (int data, Node *next) {
+ *          this->data = data;
+ *          this->next = next;
+ *      }
+ * };
+ *
+ *************************************************************************/
+int addHelper(Node* temp){
+    if (temp==NULL){
+        return 1;
+    }
+    int carry = addHelper(temp -> next);
+    temp->data += carry;
+    if(temp->data <10) return 0;
+    temp -> data = 0;
+    return 1;
+}
+Node *addOne(Node *head)
+{
+    int carry = addHelper(head);
+    if(carry == 1){
+        Node* newNode = new Node(1);
+        newNode->next = head;
+        head = newNode; // Update head to point to the new node
+    }
+    return head;
+}
