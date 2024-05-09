426e72655ab52a6b17e4a14c8181fb121ed567ac
Vipanshu Suman <vipanshu.suman@gmail.com>
2024-05-09 19:59:48 +0530

    Stacks

---
+++ Stack & Queues/Easy/Implement stack using array.cpp
@@ -0,0 +1,28 @@
+/*
+class MyStack
+{
+private:
+    int arr[1000];
+    int top;
+public:
+    MyStack(){top=-1;}
+    int pop();
+    void push(int);
+};
+*/
+
+void MyStack :: push(int x)
+{
+    top++;
+    arr[top] = x;
+}
+
+int MyStack :: pop()
+{
+    if(top==-1){
+        return -1;
+    }
+    int x = arr[top];
+    top--;
+    return x;
+}
