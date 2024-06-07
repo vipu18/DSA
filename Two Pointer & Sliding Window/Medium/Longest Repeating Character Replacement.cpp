da72e8558138f39f39fc709aeb115147f516c075
Vipanshu Suman <vipanshu.suman@gmail.com>
2024-06-07 13:21:04 +0530

    Longest Repeating Character Replacement

--- Two Pointer & Sliding Window/Medium/Longest Repeating Character Replacement.cpp
+++ Two Pointer & Sliding Window/Medium/Longest Repeating Character Replacement.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int left = 0, right = 0, maxlen = 0, maxCount = 0;
-        vector<int> hash(26, 0);
-        while (right < s.size()) {
+        int left = 0, right = 0, maxl = 0, maxf = 0;
+        vector<int> hash(26,0);
+        while(right< s.size()){
             hash[s[right] - 'A']++;
-            maxCount = max(maxCount, hash[s[right] - 'A']);
-            if (right - left + 1 - maxCount > k) {
+            maxf = max(maxf, hash[s[right] - 'A']);
+            if((right-left+1) - maxf >k){
                 hash[s[left] - 'A']--;
                 left++;
             }
-            maxlen = max(maxlen, right - left + 1);
+            if((right-left+1) - maxf <= k){
+                maxl = max (maxl, right-left+1);
+            }
             right++;
         }
-        return maxlen;
+        return maxl;
     }
 };
