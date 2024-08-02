cbd8b3ce2837fda89e10c789f6e454aede7d0767
Vipanshu Suman <vipanshu.suman@gmail.com>
2024-08-02 21:40:17 +0530

    Questions

---
+++ Practice & Daily Questions/Find Minimum Operations to Make All Elements Divisible by Three.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int n = nums.size();
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 3 != 0) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
